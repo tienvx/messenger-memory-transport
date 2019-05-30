d1ef8e94b6096f530ef4861878f1d7f7299efdc9
tien.xuan.vo <tien.xuan.vo@gmail.com>
2019-05-30 20:05:37 +0700

    Update symfony 4.3

--- src/MemoryTransportFactory.php
+++ src/MemoryTransportFactory.php
@@ -2,31 +2,32 @@
 
 namespace Tienvx\Messenger\MemoryTransport;
 
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\TransportFactoryInterface;
 use Symfony\Component\Messenger\Transport\TransportInterface;
+use Symfony\Contracts\Service\ResetInterface;
 
-class MemoryTransportFactory implements TransportFactoryInterface
+class MemoryTransportFactory implements TransportFactoryInterface, ResetInterface
 {
     /**
-     * @var Connection
+     * @var MemoryTransport[]
      */
-    private $connection;
+    private $createdTransports = [];
 
-    public function __construct(
-        Connection $connection
-    ) {
-        $this->connection = $connection;
-    }
-
-    public function createTransport(string $dsn, array $options): TransportInterface
+    public function createTransport(string $dsn, array $options, SerializerInterface $serializer): TransportInterface
     {
-        return new MemoryTransport(
-            $this->connection
-        );
+        return $this->createdTransports[] = new MemoryTransport();
     }
 
     public function supports(string $dsn, array $options): bool
     {
-        return 0 === \strpos($dsn, 'memory://');
+        return 0 === strpos($dsn, 'memory://');
+    }
+
+    public function reset()
+    {
+        foreach ($this->createdTransports as $transport) {
+            $transport->reset();
+        }
     }
 }
--- tests/MemoryTransportFactoryTest.php
+++ tests/MemoryTransportFactoryTest.php
@@ -3,7 +3,9 @@
 namespace Tienvx\Messenger\MemoryTransport\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Tienvx\Messenger\MemoryTransport\Connection;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Tests\Fixtures\DummyMessage;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Tienvx\Messenger\MemoryTransport\MemoryTransport;
 use Tienvx\Messenger\MemoryTransport\MemoryTransportFactory;
 
@@ -11,15 +13,17 @@ class MemoryTransportFactoryTest extends TestCase
 {
     public function testFactory()
     {
-        $connection = new Connection();
-        $connection->publish('the first message');
-        $connection->publish('the second message');
-        $connection->publish('the third message');
-        $connection->publish('the last message');
-
-        $factory = new MemoryTransportFactory($connection);
+        $factory = new MemoryTransportFactory();
         $this->assertTrue($factory->supports('memory://any', []));
         $this->assertFalse($factory->supports('filesystem://any', []));
-        $this->assertInstanceOf(MemoryTransport::class, $factory->createTransport('memory://any', []));
+
+        $transport = $factory->createTransport('memory://any', [], $this->createMock(SerializerInterface::class));
+        $this->assertInstanceOf(MemoryTransport::class, $transport);
+
+        $transport->send(Envelope::wrap(new DummyMessage('Hello.')));
+
+        $this->assertCount(1, $transport->get());
+        $factory->reset();
+        $this->assertCount(0, $transport->get());
     }
 }
